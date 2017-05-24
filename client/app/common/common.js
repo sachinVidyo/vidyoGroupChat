54d46692cf4b8c30d6858e3f4ae370669be3674b
Sachin Hegde <shegde@vidyo.com>
2017-05-23 21:08:46 -0700

    Fixing compilation error

--- client/app/common/common.js
+++ client/app/common/common.js
@@ -1,18 +1,16 @@
 import angular from 'angular';
 import Sidebar from './sidebar/sidebar';
-import Replaceable from './replaceable/replaceable';
 import Messageview from './messageview/messageview';
 import VideoRenderer from './videorenderer/videorenderer';
 import User from './user/user';
 
 let commonModule = angular.module('app.common', [
   Sidebar,
-  Replaceable,
   Messageview,
   VideoRenderer,
   User
 ])
-  
+
 .name;
 
 export default commonModule;
